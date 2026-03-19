f1ca5cca118f10024d9b6b1f1dff3ae56de6feb5
Daniel Legg <158085318+leggd@users.noreply.github.com>
2026-03-19 16:17:49 +0000

    Branch Tidy

--- full_scan/RiskForge/dev/adminpass.py
+++
@@ -1,7 +0,0 @@
-
-import bcrypt
-print(bcrypt.hashpw("admin".encode(), bcrypt.gensalt()).decode())
-
-# RUN THIS IN SQL SERVER WITH ABOVE PASSWORD HASH # 
-#INSERT INTO users (username, password_hash, role)
-#VALUES ('admin', 'PASTE_HASH_HERE', 'ADMIN');
